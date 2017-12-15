84fd4e713795036206494e69e7cd1303ac4826cc
Ed Gleeck <edrebg@gmail.com>
2017-12-15 12:31:51 -0500

    ldap for ranger and hdfs with group mapping using knox ldap demo

--- start.sh
+++ start.sh
@@ -2,25 +2,30 @@
 
 docker-compose -f ./docker-hdp/examples/compose/accumulo-single-container.yml up -d
 
-
+counter=0
 until $(curl --output /dev/null --silent --head --fail -u admin:admin http://localhost:8080/api/v1/clusters); do
-	echo Waiting for Ambari to start...
+	echo -ne "\\rWaiting for Ambari to start...$counter seconds elapsed"
+        ((counter=counter+5))
 	sleep 5
 done
+echo ""
 
 echo Submitting blueprint
 ./docker-hdp/submit-blueprint.sh single-container ./docker-hdp/examples/blueprints/single-container-accumulo.json
 
+echo Adding users...
+echo "Staring LDAP"
+
+docker exec -i --privileged compose_dn0.dev_1 bash /root/addLdapUsers.sh
 
 STATUS=""
+counter=0
 while [ "${STATUS}" != "STARTED" ];do 
-	echo Waiting to add policies...
+	echo -ne "\\rWaiting to add policies...$counter seconds elapsed"
+        ((counter=counter+5))
 	sleep 5
 	STATUS=`curl -k -u admin:admin -H "X-Requested-By:ambari" -s -X GET "http://localhost:8080/api/v1/clusters/dev/hosts/dn0.dev/host_components/RANGER_ADMIN" | grep "\"state\"" | cut -d'"' -f4`
 done
-
-echo Adding users...
-docker exec -it compose_dn0.dev_1 /root/addUsers.sh
 
 cd docker-hdp/containers/node/scripts
 
@@ -34,8 +39,10 @@ cd -
 
 
 STATUS=""
+counter=0
 while [ "${STATUS}" != "STARTED" ];do
-        echo Waiting to insert test table...
+        echo -ne "\\rWaiting to insert test table... $counter seconds elapsed"
+        ((counter=counter+5))
         sleep 5
         STATUS=`curl -k -u admin:admin -H "X-Requested-By:ambari" -s -X GET "http://localhost:8080/api/v1/clusters/dev/hosts/dn0.dev/host_components/ACCUMULO_TRACER" | grep "\"state\"" | cut -d'"' -f4`
 done
